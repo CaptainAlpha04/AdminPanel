00a0c3c594a37e56427479857ae810f58f0f209e
CaptainAlpha04 <aliimran.iam59@gmail.com>
2024-05-17 09:09:00 +0500

    Refactor student route to include API endpoints for student data retrieval

--- server.mjs
+++ server.mjs
@@ -36,7 +36,7 @@ const PORT = process.env.PORT || 5000
 mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true')
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.log(error))
-
+    
    // Create a MySQL connection using the connection details from the environment variables
    const connection = createConnection({
      host: process.env.MYSQL_HOST,
